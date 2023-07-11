a4b04b37eedf87287adbbcabbc1545c64aa0f24b
Vagner Bruno da Silva <vagnerb1987@gmail.com>
2023-07-10 21:43:29 -0300

    Excluindo exercicio 149

--- 149-Exercicio_Resolvido/src/entities/enums/WorkerLevel.java
+++
@@ -1,9 +0,0 @@
-package entities.enums;
-
-public enum WorkerLevel {
-	/*--Classe em que posso separar o nivel dos trabalhadores, assim como etapas de um processo
-	de vendas por ex:. aguardando pagamento, pagamento recebido, preparando pedido, pedido enviado, etc..*/
-	JUNIOR,
-	MID_LEVEL,
-	SENIOR;
-}
